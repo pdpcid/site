df4da41ee1f9ebed7705c1a919f6b517678d5293
Riza Fahmi <rizafahmi@gmail.com>
2018-08-29 07:06:25 +0700

    Add global css

--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <div>
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
+            color: 'black',
             textDecoration: 'none',
           }}
         >
--- src/components/layout.js
+++ src/components/layout.js
@@ -26,19 +26,13 @@ const Layout = ({ children }) => (
             { name: 'keywords', content: 'sample, something' },
           ]}
         >
+          <link rel="stylesheet" type="text/css" href="./normalize.css" />
+          <link rel="stylesheet" type="text/css" href="./style.min.css" />
+          <link rel="stylesheet" type="text/css" href="./styles.css" />
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <div>{children}</div>
       </>
     )}
   />
